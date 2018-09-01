ceca972bc314dd8c0d5653176a58a217182d4b8a
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-01 11:25:37 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,9 @@ class Backer
   
   
   
-  ::new
-  takes a name on initialization
-  end 
+  # ::new
+  # takes a name on initialization
+  # end 
   
   def back_project
   # accepts a Project as an argument and stores it in a backed_projects array
