d0cda6be300aec87992ed6a4de64000f1a8d20a4
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-01 11:31:23 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Project
-  attr_accessor :title, :backed_projects, :baker
+  attr_accessor :title, :backer
   
   def title(title)
     @title = title 
-    @backed_projects = []
+    
   end 
   
   
