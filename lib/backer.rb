8f1af2a97e7deba86010d4c4b92c550e88c5c6d4
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-01 11:35:05 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,17 @@
 class Backer
   attr_accessor :name, :backed_projects
   
+  # ::new
+  # takes a name on initialization
   def initialize(name)
     @name = name
     @backed_projects = []
   end 
-  
-
   
   def back_project(project)
     @backed_projects << project
   # # accepts a Project as an argument and stores it in a backed_projects array
   # # also adds the backer to the project's backers array
   end
+
 end
