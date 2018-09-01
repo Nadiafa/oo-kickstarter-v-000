6d74d3b9406a61194ccbe69da0b36b2415b64a27
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-01 11:37:17 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,12 +5,13 @@ class Project
   # takes a name on initialization
   def initialize(title)
     @title = title
+    @backer = []
   end  
   
   # #add_backer
   # accepts a Backer as an argument and stores it in a backers array
     def add_backer(baker)
-      @baker
+      @backer << 
   # # accepts a Backer as an argument and stores it in a backers array
   # # also adds the project to the backer's backed_projects array
   # end
