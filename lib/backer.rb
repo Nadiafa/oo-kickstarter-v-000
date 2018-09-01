5f7033f7a33abfd9d266cdb47ae3f156d308384b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-01 12:01:41 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,11 +6,8 @@ class Backer
     @backed_projects = []
   end 
   
-  # #back_project
-  # # accepts a Project as an argument and stores it in a backed_projects array
   def back_project(project)
     @backed_projects << project
-  # # also adds the backer to the project's backers array
     project.add_backer(self)
   end
 
